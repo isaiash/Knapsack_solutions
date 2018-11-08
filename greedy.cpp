8dfc338b9ca5638e0343de32158057223b7f7f95
Daniel Neira <dneira_94@hotmail.com>
2018-11-08 00:23:54 -0300

    Writer to HD added

--- greedy.cpp
+++ greedy.cpp
@@ -13,6 +13,8 @@
 #include <ctime>
 #include <algorithm>
 
+#define item_en_mochila 500
+
 typedef std::vector<std::pair<int, int> > ItemVector;
 
 struct inst{
@@ -22,14 +24,12 @@ struct inst{
     int num_items;
 };
 struct soln{
-    uint64_t config;
+    std::bitset<item_en_mochila> config;
     int id;
     int num_items;
     int cost;
+    double time;
 };
-
-int bbTopCost;
-std::vector<int> bbTopConfig;
 
 //helpers
 template<typename T>
@@ -48,8 +48,8 @@ void printInst(inst instance){
 }
 
 void printSoln(soln solution){
-    printf("Id: %d, n: %d, cost: %d, config: %lld\n", solution.id, solution.num_items, solution.cost, solution.config);
-    //std::cout << "sol: " << toBinaryString(solution.config) << std::endl;
+    printf("Id: %d, n: %d, cost: %d\n", solution.id, solution.num_items, solution.cost);
+    std::cout << "sol: " << solution.config << std::endl;
 }
 
 int GetInstances(std::vector<inst> &instances, const char* filename, bool verbose){
@@ -81,12 +81,9 @@ int GetInstances(std::vector<inst> &instances, const char* filename, bool verbos
 
 int DropCostBits(std::vector<inst> &instances, int numBits, bool verbose){
     if (numBits == 0) return 0;
-    for (int i = 0; i < instances.size(); i++)
-    {
+    for (int i = 0; i < instances.size(); i++){
         if (verbose) printf("Assigning instance %d items to bins...\n", i);
-        
-        for (int j = 0; j < instances[i].num_items; j++)
-        {
+        for (int j = 0; j < instances[i].num_items; j++){
             instances[i].items[j].second = instances[i].items[j].second & ~((int)std::pow(2, numBits) - 1);
         }
     }
@@ -95,12 +92,9 @@ int DropCostBits(std::vector<inst> &instances, int numBits, bool verbose){
 
 int RoundCosts(std::vector<inst> &instances, int binSize, bool verbose){
     if (binSize == 1) return 0;
-    for (int i = 0; i < instances.size(); i++)
-    {
+    for (int i = 0; i < instances.size(); i++){
         if (verbose) printf("Assigning instance %d items to bins...\n", i);
-        
-        for (int j = 0; j < instances[i].num_items; j++)
-        {
+        for (int j = 0; j < instances[i].num_items; j++){
             int margin = binSize - (instances[i].items[j].second % binSize);
             instances[i].items[j].second += margin;
             if (verbose) printf("Rounding item %d cost up by %d\n", j, margin);
@@ -114,26 +108,20 @@ int Greeeeeedy(std::vector<inst> instances, std::vector<soln> &solutions, bool v
     //start timer
     std::clock_t start;
     double duration;
-    
     start = std::clock();
-    
     //process
     while (!instances.empty()){
         inst curInst = instances.back();
         instances.pop_back();
-        
         int solnWeight = 0;
         int solnCost = 0;
-        std::bitset<40> solnConfig;
-        
+        std::bitset<item_en_mochila> solnConfig;
         for (int j = 0; j < curInst.num_items; j++){
             int topCWR = 0;
             int topCWRindex = -1;
-            
             for (int i = 0; i < curInst.num_items; i++){//find highest CWR
                 int curCWR = curInst.items[i].second / curInst.items[i].first;
-                if (curCWR > topCWR && solnWeight + curInst.items[i].first <= curInst.max_weight && !solnConfig.test(i))
-                {
+                if (curCWR > topCWR && solnWeight + curInst.items[i].first <= curInst.max_weight && !solnConfig.test(i)){
                     topCWR = curCWR;
                     topCWRindex = i;
                 }
@@ -144,21 +132,42 @@ int Greeeeeedy(std::vector<inst> instances, std::vector<soln> &solutions, bool v
                 solnConfig.set(topCWRindex);
             }
         }
-        
+        //stop timer
+        duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+        printf("Tiempo de resolución instancia %d: %f\n", curInst.id ,duration);
         soln newSoln = {
-            .config     = solnConfig.to_ullong(),
+            .config     = solnConfig,
             .id         = curInst.id,
             .num_items     = curInst.num_items,
-            .cost         = solnCost
+            .cost         = solnCost,
+            .time         = duration
         };
         solutions.push_back(newSoln);
         if (verbose) printSoln(newSoln);
     }
-    
-    //stop timer
-    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    printf("%f\n", duration);
-    
+    return 0;
+}
+
+int WriteSolution(std::vector<soln> &solutions, bool verbose){
+    if (verbose) printf("Escribiendo instancias..\n");
+    std::ofstream myfile;
+    std::string name;
+    for(unsigned int i = 0; i < solutions.size(); i++){
+        name = std::to_string(solutions[i].id);
+        name += "_greedy.txt";
+        myfile.open(name, std::fstream::in | std::fstream::out | std::fstream::app);
+        if (!myfile.is_open()) {
+            if (verbose) printf("Archivo no fue encontrado o no existe, creando archivo con tal nombre...\n");
+            myfile.open(name,  std::fstream::in | std::fstream::out | std::fstream::trunc);
+            myfile << solutions[i].id <<"\t"<<solutions[i].cost<<"\t"<<solutions[i].time<<"\t"<<std::endl;
+            myfile.close();
+        }
+        else{
+            if (verbose) printf("Archivo encontrado, comenzando grabado.\n");
+            myfile << solutions[i].id <<"\t"<<solutions[i].cost<<"\t"<<solutions[i].time<<"\t"<<std::endl;
+            myfile.close();
+        }
+    }
     return 0;
 }
 
@@ -171,12 +180,10 @@ int main(int argc, const char** argv){
     verbose = true;
 
     GetInstances(instances, argv[1], verbose);
-    
     Greeeeeedy(instances, solutions, verbose);
-    
+    WriteSolution(solutions,verbose);
     instances.clear();
     solutions.clear();
-    bbTopConfig.clear();
     
     return 0;
 }

f4a28b0c726c6dcc807e2203adde107cc1b6b469
Daniel Neira <dneira_94@hotmail.com>
2019-05-24 16:58:28 -0400

    update ga

--- codes/ga_base.cpp
+++ codes/ga_base.cpp
@@ -3,15 +3,19 @@
 #include<stdlib.h>
 #include<time.h>
 #include<iostream>
-#include <fstream>
-#include <vector>
 
-#define maxit 50
-#define mintimetoprint 0.0001
+#define n 10000		// number of items
+#define p 25		// density of knapsack problem
+#define m n
+#define maxit 30
+
+FILE *f;
 unsigned int b,i,j,k,it=1,e1,e2,E,maxb,maxO=0;
-double O,lastprint=0.0;
+unsigned int a[n+1]={0},c[n+1]={0},ri[n+1]={0},pop[m+1][n+1]={0},popf[m+1]={0},popfi[m+1]={0},pop2[m+1][n+1]={0},pop2f[m+1]={0};
+double r[n+1]={0},rf[m+1]={0},O;
+char fins[12],ns[8],ps[8];
 
-unsigned int pivotlistf(unsigned int first,unsigned int last, std::vector<double> &r, std::vector<int> &ri){
+unsigned int pivotlistf(unsigned int first,unsigned int last){
 
   double pivotvalue=0.0,tempr=0.0;
   unsigned int pivotpoint=0,index=0,tempw=0;
@@ -29,6 +33,7 @@ unsigned int pivotlistf(unsigned int first,unsigned int last, std::vector<double
     tempw=ri[pivotpoint];
     ri[pivotpoint]=ri[index];
     ri[index]=tempw;
+
    }
    tempr=r[first];
    r[first]=r[pivotpoint];
@@ -41,16 +46,16 @@ unsigned int pivotlistf(unsigned int first,unsigned int last, std::vector<double
    return pivotpoint;
  }
 
-void quicksortf(unsigned int first,unsigned int last, std::vector<double> &r, std::vector<int> &ri){
+void quicksortf(unsigned int first,unsigned int last){
  unsigned int pivot=0;
  if (first<last){
-  pivot=pivotlistf(first,last,r,ri);
-  quicksortf(first,pivot-1,r,ri);
-  quicksortf(pivot+1,last,r,ri);
+  pivot=pivotlistf(first,last);
+  quicksortf(first,pivot-1);
+  quicksortf(pivot+1,last);
  }
 }
 
-unsigned int pivotlisti(unsigned int first,unsigned int last, std::vector<int> &popf,std::vector<int> &popfi, std::vector<double> &r){
+unsigned int pivotlisti(unsigned int first,unsigned int last){
   unsigned int pivotvalue=0,tempi=0;
   unsigned int pivotpoint=0,index=0,tempw=0;
   pivotvalue=popf[first];
@@ -79,59 +84,49 @@ unsigned int pivotlisti(unsigned int first,unsigned int last, std::vector<int> &
    return pivotpoint;
  }
 
-void quicksorti(unsigned int first,unsigned int last, std::vector<int> &popf,std::vector<int> &popfi, std::vector<double> &r){
+void quicksorti(unsigned int first,unsigned int last){
  unsigned int pivot=0;
-    if (first<last){
-        pivot=pivotlisti(first,last,popf,popfi,r);
-        quicksorti(first,pivot-1,popf,popfi,r);
-        quicksorti(pivot+1,last,popf,popfi,r);
-    }
+
+ if (first<last){
+  pivot=pivotlisti(first,last);
+  quicksorti(first,pivot-1);
+  quicksorti(pivot+1,last);
+ }
 }
 
-int main(int argc, char *argv[]){
-	int n, id;
-	if(argc != 2){
-        std::cerr<<"Usage: "<<argv[0]<<" 'instace file name'"<<std::endl;
-        return 0;
-    }
-    std::ifstream infile(argv[1]);
+int main(){
+	//itoa(n,ns,10);
+	//itoa(p,ps,10);
+	std::string sns = std::to_string(n);
+	strcpy(ns, sns.c_str());
+	std::string sps = std::to_string(p);
+	strcpy(ps, sps.c_str());
+	strcpy(fins,"");
+	strncat(fins,ns,strlen(ns)+1);
+	strncat(fins,"_",strlen(ns)+2);	
+	strncat(fins,ps,strlen(ps)+1);
+	strncat(fins,".txt",strlen(ns)+1+strlen(ps)+1);
+	f=fopen(fins,"r");
+	fscanf(f,"%d",&i);
+    if (i!=n){
+		printf("¡El tamaño es incompatible!");
+		return 0;
+	}
+	for(i=1; i<=n; i++)
+		fscanf(f,"%d %d",&a[i],&c[i]);
+	fscanf(f,"%d",&b);
+	fclose(f);
+	clock_t start, end;
+	start = clock();
 
-    infile>>id; //id of file - instance
-    infile>>n; //number of items
-    infile>>b; //capacity
+	for(i=1;i<=n;i++){
+		r[i]=1.0*c[i]/a[i];
+		ri[i]=i;
+	}
+	quicksortf(1,n);
 
-    std::vector<int> c(n+1,0); //value
-    std::vector<int> a(n+1,0); //weight
-    std::vector<double> r(n+1,0.0);//density = value/weight =c/a
-    std::vector<int> ri(n+1,0); //density order
-    int it_weight, it_value;
-    double it_density;
-    for(int i=1; i<=n; i++){
-            infile>>it_weight;
-            a[i] = it_weight;
-    }
-    
-    unsigned int m = n;
-
-    for(int i=1; i<=n; i++){
-        infile >> it_value;
-        it_density=it_value/(double) a[i];
-        c[i] = it_value;
-        r[i] = it_density;
-        ri[i] = i;
-    }
-
-    std::vector< std::vector<int> > pop(m+1, std::vector<int> (n+1,0));
-    std::vector< std::vector<int> > pop2(m+1, std::vector<int> (n+1,0));
-    std::vector<int> popf(m+1,0);
-    std::vector<int> popfi(m+1,0);
-    std::vector<int> pop2f(m+1,0);
-    std::vector<double> rf(n+1,0);
-
-    clock_t start = clock();
-	quicksortf(1,n,r,ri);
-    
 	srand( (unsigned)time( NULL ) );
+
 	for(k=1;k<=m;k++){
 		for(i=k;i<=n;i++)
 			if (pop[k][0] + a[ri[i]] <= b){
@@ -149,6 +144,7 @@ int main(int argc, char *argv[]){
 				popf[k]+=c[i];
 			}
 		}
+
 		for(i=n; i>=1 && pop[k][0]>b; i--)
 			if (pop[k][ri[i]]==1){
 				pop[k][ri[i]]=0;
@@ -161,12 +157,12 @@ iterasyon:
 	for(i=1;i<=m;i++)
 		popfi[i]=i;
 
-	quicksorti(1,m, popf, popfi,r);
+	quicksorti(1,m);
 
 	for(k=1;k<=m/2;k++){
 		for(j=0;j<=n;j++)
-			pop2[k][j] = pop[popfi[k]][j];
-		pop2f[k] = popf[k];
+			pop2[k][j]=pop[popfi[k]][j];
+		pop2f[k]=popf[k];
 	}
 
 	popf[0]=0;
@@ -174,7 +170,7 @@ iterasyon:
 		popf[0]+=popf[i];
 	rf[0]=0;
 	for(i=1;i<=m;i++)
-		rf[i] = rf[i-1] + (100.0*popf[i]/popf[0]);
+		rf[i]=rf[i-1] + (100.0*popf[i]/popf[0]);
 	
 	for(k=m/2+1; k<=m; k++){
 		i=(rand() % 100)+1;
@@ -182,14 +178,15 @@ iterasyon:
 		while (i>rf[e1] && e1<m)
 			e1++;
 yenie2:
-		i=(rand() % 100) + 1;
+		i=(rand() % 100)+1;
 		e2=0;
 		while (i>rf[e2] && e2<m)
 			e2++;
 		if (e1==e2)
 			goto yenie2;
-		E = popf[e1]+popf[e2];
-		O = 100*popf[e1]/E;
+
+		E=popf[e1]+popf[e2];
+		O=100*popf[e1]/E;
 		pop2[k][0]=0;
 		pop2f[k]=0;
 		for(j=1;j<=n;j++){
@@ -215,14 +212,11 @@ yenie2:
 		popf[k]=pop2f[k];
 		for(j=0; j<=n; j++)
 			pop[k][j]=pop2[k][j];
+
 		if (popf[k]>maxO){
 			maxO=popf[k];
 			maxb=pop[popfi[k]][0];
-			//printf("it : %d  b : %d  maxO : %d\n",it,maxb,maxO);
-            if((float) (clock() - lastprint) / CLOCKS_PER_SEC > mintimetoprint){
-                printf("%d   %d  %f\n", id, maxO, (float) (clock() - start) / CLOCKS_PER_SEC);
-                lastprint = clock();
-            }
+			printf("it : %d  b : %d  maxO : %d\n",it,maxb,maxO);
 		}
 	}
 
@@ -231,28 +225,32 @@ yenie2:
 		j=1;
 		while (j<=n/4){
 			i=(rand() % n)+1;
-			if (pop[k][i]==1){
+			if (pop[k][i]==1)
+			{
 				pop[k][i]=0;
 				pop[k][0]-=a[i];
 				popf[k]-=c[i];
 			}
 			j++;
 		}
+	
 		for(i=1; i<=n && pop[k][0]<=b; i++)
-			if (pop[k][ri[i]]==0){
+			if (pop[k][ri[i]]==0)
+			{
 				pop[k][ri[i]]=1;
 				pop[k][0]+=a[ri[i]];
 				popf[k]+=c[ri[i]];
 			}
 
 		for(i=n; i>=1 && pop[k][0]>b; i--)
-			if (pop[k][ri[i]]==1){
+			if (pop[k][ri[i]]==1)
+			{
 				pop[k][ri[i]]=0;
 				pop[k][0]-=a[ri[i]];
 				popf[k]-=c[ri[i]];
 			}	
 	}
 	it++;
-	if (it < maxit) goto iterasyon;
-	//printf("\nTime : %f\n\n", (float) (clock() - start) / CLOCKS_PER_SEC);
+	if (it<maxit) goto iterasyon;
+	printf("\nTime : %f\n\n", (float) (clock() - start) / CLOCKS_PER_SEC);
 }
